88c6e94e0c0c6b79ffa6592ca2945c53dd869662
encinafr <36193725+encinafr@users.noreply.github.com>
2018-04-11 11:55:53 -0300

    le agregue edad

--- cadenascartacteres/main.c
+++ cadenascartacteres/main.c
@@ -5,9 +5,11 @@
 int main()
 {
     int i,j;
-    char nombre[ELEMENTOS][50] = {"zapallo","Carlos","Juan","Ernesto","Mario"};
+    char nombre[ELEMENTOS][50] = {"Juan","Carlos","Ana","Ernesto","Mario"};
     char apellido[ELEMENTOS][50]= {"Gomez","Fernandez","Paz","Perez","Lopez"};
+    int edad[ELEMENTOS]={20,30,50,10,21};
     char auxiliar[50];
+    int aux;
     for(i=0;i< ELEMENTOS-1;i++)
     {
     for(j=i+1;j<ELEMENTOS;j++)
@@ -17,16 +19,23 @@ int main()
     strcpy(auxiliar,nombre[i]);
     strcpy(nombre[i],nombre[j]);
     strcpy(nombre[j], auxiliar);
+
     strcpy(auxiliar,apellido[i]);
     strcpy(apellido[i],apellido[j]);
     strcpy(apellido[j], auxiliar);
+
+    aux=edad[i];
+    edad[i]=edad[j];
+    edad[j]=aux;
+
+    //para cada dato tengo que repetir las tres lineas para que me mueva el nombre con el apellido y el otro dato que le agregue
     }
     }
 }
 printf("\n\nLista ordenada por nombres");
 for(i=0;i<ELEMENTOS;i++)
 {
-printf("\nNombre: %s - Apellido: %s",nombre[i],apellido[i]);
+printf("\nNombre: %s - Apellido: %s - Edad %d",nombre[i],apellido[i],edad[i]);
 }
 return 0;
 }
