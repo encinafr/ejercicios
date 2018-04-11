7c7c78d61926383b9f9ed7a8f263e510fbc55d79
encinafr <36193725+encinafr@users.noreply.github.com>
2018-04-11 10:50:28 -0300

    Add files via upload

--- matriz/main.c
+++ matriz/main.c
@@ -6,16 +6,19 @@ int main()
   int i;
   int legajo[CANT];
   char nombre[CANT][31];
+  float salario[CANT];
   for(i=0;i<CANT;i++){
   legajo[i]=i+1;
   printf("\nnombre:");
   fflush(stdin);
   scanf("%[^\n]",nombre[i]);
+  printf("salario");
+  scanf("%f",&salario[i]);
 
   }
-  printf("\nlegajo: \tnombre\n");
+  printf("\nlegajo: \tnombre\t salario");
   for(i=0; i<CANT;i ++){
-    printf("\n %d \t\t %s",legajo[i],nombre[i]);
+    printf("\n %d \t\t %s \t %.2f",legajo[i],nombre[i],salario[i]);
   }
     return 0;
 }
