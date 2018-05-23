8375059264de41d8e81dcc1d335f4a437f4e45c2
encinafr <36193725+encinafr@users.noreply.github.com>
2018-05-23 11:12:02 -0300

    Terminar

---
+++ People/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Person.h"
+
+
+int main()
+{
+    int edad;
+    Person* personArray[10];
+    int i;
+    printf("ingrese edad:");
+        scanf("%d",&edad);
+    for(i = 0; i < 10; i++)
+    {
+        personArray[i] = person_new(edad,i);
+    }
+
+    for(i = 0; i < 0; i++)
+    {
+
+        person_setAge(personArray[i],edad);
+    }
+    for(i = 0; i < 10; i++)
+    {
+        printf("\nAge: %2d",person_getAge(personArray[i]));
+    }
+    scanf(" ");
+    return 0;
+}
+
