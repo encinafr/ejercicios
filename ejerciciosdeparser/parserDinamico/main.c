ab0b38c23ce8957ddaaa4821615c4e9df64682fd
encinafr <36193725+encinafr@users.noreply.github.com>
2018-06-04 11:58:09 -0300

    Add files via upload

--- ejerciciosdeparser/parserDinamico/main.c
+++ ejerciciosdeparser/parserDinamico/main.c
@@ -11,15 +11,15 @@ typedef struct{
 int main()
 {
    int listSize=20;
-   eAutos p;
+    eAutos p;
     eAutos* autos = (eAutos*)malloc(sizeof(eAutos)*listSize);
     int index=0;
     int i;
-     FILE *f;
- int cant;
- int id,anio;
- char var1[50],var3[50],var2[50],var4[50],var5[50];
- f = fopen("autos.csv","r");
+    FILE *f;
+    int cant;
+    int id,anio;
+    char var1[50],var3[50],var2[50],var4[50],var5[50];
+    f = fopen("autos.csv","r");
  if(f==NULL){
         printf("No se pudo abrir el archivo\n");
         exit(EXIT_FAILURE);
@@ -28,11 +28,11 @@ int main()
 
         cant= fscanf(f,"%[^,],%[^,],%[^,],%[^,],%[^\n]\n",var1,var2,var3,var4,var5);
         for(i=0;i<cant;i++){
-            autos[i].anio=atoi(var5);
-            autos[i].id=atoi(var1);
-            strcpy(autos[i].Marca,var2);
-            strcpy(autos[i].Modelo,var3);
-            strcpy(autos[i].color,var4);
+            (autos+i)->anio=atoi(var5);
+            (autos+i)->id=atoi(var1);
+            strcpy((autos+i)->Marca,var2);
+            strcpy((autos+i)->Modelo,var3);
+            strcpy((autos+i)->color,var4);
         }
 
        autos[index]=p; // copiamos "p" a una posicion del array de estructuras.
@@ -58,7 +58,7 @@ int main()
         }
         for(i=0;i<cant;i++){
 
-            printf("Id %4d Marca %18s Modelo %10s Color %5s Anio %4d\n",autos[i].id,autos[i].Marca,autos[i].Modelo,autos[i].color,autos[i].anio);
+            printf("Id %4d Marca %18s Modelo %10s Color %5s Anio %4d\n",(autos+i)->id,(autos+i)->Marca,(autos+i)->Modelo,(autos+i)->color,(autos+i)->anio);
             break;
         }
     }
