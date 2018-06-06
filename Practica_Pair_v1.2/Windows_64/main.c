c620c5e1e6a19585b5db2c5be8aaa841a590228e
encinafr <36193725+encinafr@users.noreply.github.com>
2018-06-06 12:21:10 -0300

    Add files via upload

--- Practica_Pair_v1.2/Windows_64/main.c
+++ Practica_Pair_v1.2/Windows_64/main.c
@@ -17,61 +17,6 @@
 int main()
 {
 
-//hacer un swicht
-   // funcio
-    char seguir='s';
-    int opcion=0;
-
-
-    while(seguir=='s')
-
-    {
-        system("cls");
-        printf("\n\n *******MENU*******\n\n");
-        printf("1. Parse del archivo data.csv\n");
-        printf("2. Listar Empleados\n");
-        printf("3. Ordenar por nombre\n");
-        printf("4. Agregar un elemento\n");
-        printf("5. Elimina un elemento\n");
-        printf("6. Listar Empleados (Desde/Hasta)\n");
-        printf("7. Salir\n");
-        printf("\n\n*****************************************************\n");
-
-        scanf("%d",&opcion);
-
-        switch(opcion)
-        {
-            case 1:
-
-                break;
-
-            case 2:
-
-                break;
-            case 3:
-
-
-                break;
-            case 4:
-
-                break;
-            case 5:
-
-                break;
-            case 6:
-
-                break;
-
-                break;
-            case 7:
-                seguir = 'n';
-                break;
-            default:
-                printf("A ingresado una opcion incorrecta \n");
-                system("pause");
-        }
-
-    }
 
     return 0;
 }
